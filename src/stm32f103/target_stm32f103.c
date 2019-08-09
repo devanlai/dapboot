721650827b90c7d3a7a601a94f31134374b3ff96
Tercio Gaudencio Filho <terciofilho@gmail.com>
2019-08-09 13:22:38 -0300

    Added a variable to control the button sample delay.

--- src/stm32f103/target_stm32f103.c
+++ src/stm32f103/target_stm32f103.c
@@ -162,7 +162,7 @@ bool target_get_force_bootloader(void) {
 
 #if HAVE_BUTTON
     /* Wait sometime in case the button has some debounce capacitor */
-    for (int i = 0; i < 1440000; i++) {
+    for (int i = 0; i < BUTTON_SAMPLE_DELAY_CYCLES; i++) {
         __asm__("nop");
     }
     /* Check if the user button is held down */
